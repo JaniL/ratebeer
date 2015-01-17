fd18f968108f3824a80212bb6d4b7a409775dd58
Jani Luukko <jj.luukko@gmail.com>
2015-01-18 01:48:16 +0200

    t13&14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,6 @@
 class Brewery < ActiveRecord::Base
-  has_many :beers
+  has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
 
   def print_report
     puts name
@@ -10,5 +11,9 @@ class Brewery < ActiveRecord::Base
   def restart
     self.year = 2015
     puts "changed year to #{self.year}"
+  end
+
+  def average_rating
+    self.ratings.average(:score).round(1)
   end
 end
