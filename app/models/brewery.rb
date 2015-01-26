10e37632a38dd462bbc78217722fdf6c69a9839f
Jani Luukko <jj.luukko@gmail.com>
2015-01-26 02:04:29 +0200

    tehtävä 5

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,6 +3,9 @@ class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
+  validates :name, presence: true
+  validates_inclusion_of :year, :in => 1042..2015
+
   def print_report
     puts name
     puts "established at year #{year}"
