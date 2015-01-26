346aa268500ad6ee9794a55a8e9b04dbf4c1faf3
Jani Luukko <jj.luukko@gmail.com>
2015-01-26 02:18:29 +0200

    tehtävä 6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,7 +4,22 @@ class Brewery < ActiveRecord::Base
   has_many :ratings, through: :beers
 
   validates :name, presence: true
-  validates_inclusion_of :year, :in => 1042..2015
+  # validates_inclusion_of :year, :in => 1042..2015
+
+  validate :year_cannot_be_in_the_future
+  validate :year_cannot_be_so_old_pls
+
+  def year_cannot_be_in_the_future
+    if year > Time.now.year
+      errors.add(:year, "can't be in the future")
+    end
+  end
+
+  def year_cannot_be_so_old_pls
+    if year < 1042
+      errors.add(:year, "can't be so far away pls")
+    end
+  end
 
   def print_report
     puts name
