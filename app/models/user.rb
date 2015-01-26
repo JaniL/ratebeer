907ba3136faf862d586cd492b7d1391de28e25e4
Jani Luukko <jj.luukko@gmail.com>
2015-01-26 17:45:43 +0200

    tehtävä 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ActiveRecord::Base
   has_many :beers, through: :ratings
   has_many :memberships
   has_many :beer_clubs, through: :memberships
+
+  has_secure_password
+
+  validates :password, length: { minimum: 4}
+  validates_format_of :password, :with => /[A-Z\d]/, message: "doesn't contain at least one uppercase character and/or a number."
 end
